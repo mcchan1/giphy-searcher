9a32df6710d990cdb0618fdd9671f6b351131be7
mcchan1 <mcchan1@gmail.com>
2016-04-10 15:09:52 -0400

    login user added to template
    only logged in users can view. need to individualize per user

--- collections.js
+++ collections.js
@@ -2,3 +2,4 @@ Photographs = new Mongo.Collection('photographs');
 //All the data gathered from instagram post will be collected. 
 //Publication will filter out data to be made available to the Subsctiption
 //loadPictures function will choose data from the Subsctiption to be displayed in template
+CustomerList = new Mongo.Collection('customer');
